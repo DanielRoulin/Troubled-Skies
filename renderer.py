0acb9ee98ae18b3ec20e54769aae83664986405e
Daniel Roulin <dadaroulin@bluewin.ch>
2022-10-11 21:10:18 +0200

    Finished renderer

--- renderer.py
+++ renderer.py
@@ -138,31 +138,35 @@ class HUSCIIRenderer:
     def text(self, x, y, s):
         """Write the text s statring at the x, y location on the screen."""
         if y >= 0 and y < self.HEIGHT and x < self.WIDTH and x > -len(s):
-            start = min(0, x)
-            stop = min(len(s), self.WIDTH - x)
-            print(start, stop)
-            print(s[start:stop])
-            self.chars[y, max(0, x):max(x + len(s), self.WIDTH)] = list(s[start:stop])
+            start = -x if x < 0 else 0
+            stop = self.WIDTH - x if x > self.WIDTH-len(s) else len(s)
+            self.chars[y, max(0, x):min(x + len(s), self.WIDTH)] = list(s[start:stop])
         
-        
+
+    @round_inputs
     def sprite(self, x, y, chars):
-        """Draws the 2d array of chars on the screen, (x, y) determines the top left location."""        
+        """Draws the 2d array of chars on the screen, (x, y) determines the top left location."""
         maxw = max(len(line) for line in chars)
         chars = np.array([list(line.ljust(maxw)) for line in chars], dtype = self.dtype)
-        w, h = chars.shape
-        self.chars[x:x+w, y:y+h] = chars
+        h, w = chars.shape
+        if y > -h and y < self.HEIGHT and x < self.WIDTH and x > -w:
+            start_x = -x if x < 0 else 0
+            stop_x = self.WIDTH - x if x > self.WIDTH-w else w
+            start_y = -y if y < 0 else 0
+            stop_y = self.HEIGHT - y if y > self.HEIGHT-h else h
+            self.chars[max(0, y):min(y + h, self.HEIGHT), max(0, x):min(x + w, self.WIDTH)] = chars[start_y:stop_y, start_x:stop_x]
         
     
 
-if __name__ == "__main__":
-    r = HUSCIIRenderer()
-#     r.rect(40, 10, 50, 10, "O")
-#     r.ellipse(40, 12, 10, 5, "O")
-#     r.text(60,20,"Hello World!")
-#     r.line(3, 5, 70, 20, "O")
-#     r.sprite(77, 23, [" O O ",
-#                     "\   /",
-#                     " --- "])
-    r.text(-5, 24, "ABCDEFGHIJ")
-#     r.rect(-15, -25, 10, 10, "A")
-    r.draw()
+# if __name__ == "__main__":
+    # r = HUSCIIRenderer()
+    # # r.rect(40, 10, 50, 10, "O")
+    # # r.ellipse(40, 12, 20, 5, "O")
+    # # r.text(60,20,"Hello World!")
+    # # r.line(3, 5, 70, 20, "O")
+    # r.sprite(0, 30, [" O O ",
+    #                  "\   /",
+    #                  " --- "])
+    # # r.text(75, 24, "ABCDEFGHIJ")
+    # # r.rect(-15, -25, 10, 10, "A")
+    # r.draw()
